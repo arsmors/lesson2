fbd76c37f4e08b9a21c717a4f7affb4d48af750e
Arsens <arsens.morins@chi.lv>
2018-12-03 20:38:52 +0200

    first

---
+++ app/src/main/java/com/morinscompany/lesson2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.morinscompany.lesson2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginOnClick(View view) {
+
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        String emailField = email.getText().toString();
+        String passwordField = password.getText().toString();
+
+        if ((emailField == null) || (passwordField == null)) {
+            return;
+        }
+
+        if ((emailField != null) || (passwordField != null)) {
+
+            int a = 0;
+        }
+    }
+}
