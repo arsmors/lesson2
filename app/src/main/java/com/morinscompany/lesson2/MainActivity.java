74916e3fb28b8bfcef0005b4e932f2cfeb8deaef
Arsens <arsens.morins@chi.lv>
2018-12-05 21:52:35 +0200

    header

--- app/src/main/java/com/morinscompany/lesson2/MainActivity.java
+++ app/src/main/java/com/morinscompany/lesson2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.morinscompany.lesson2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,19 +16,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginOnClick(View view) {
 
-        EditText email = (EditText)findViewById(R.id.email);
-        EditText password = (EditText)findViewById(R.id.password);
+//        EditText email = (EditText)findViewById(R.id.email);
+//        EditText password = (EditText)findViewById(R.id.password);
+//
+//        String emailField = email.getText().toString();
+//        String passwordField = password.getText().toString();
+//
+//        if ((emailField == null) || (passwordField == null)) {
+//            return;
+//        }
+//
+//        if ((emailField != null) || (passwordField != null)) {
+//
+//            int a = 0;
+//        }
 
-        String emailField = email.getText().toString();
-        String passwordField = password.getText().toString();
-
-        if ((emailField == null) || (passwordField == null)) {
-            return;
-        }
-
-        if ((emailField != null) || (passwordField != null)) {
-
-            int a = 0;
-        }
+        Intent intent = new Intent(this, Header.class);
+        startActivity(intent);
     }
 }
